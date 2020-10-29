415c32ad17eea6eb71a3addeb21c7c7a15006132
Fernando Silva Kroes <meanmachine200+github@gmail.com>
2020-10-29 11:10:17 +0100

    used new method of async handling in react

--- src/App.js
+++ src/App.js
@@ -12,14 +12,25 @@ class App extends React.Component {
   }
 
   callAPI() {
-    fetch('http://localhost:9000/testAPI')
-      .then(res => res.text())
-      .then(res => this.setState({ apiResponse: res }))
-      .catch(err => err);
+    return fetch('http://localhost:9000/testAPI')
+      // .then(res => res.text())
+      // .then(res => this.setState({ apiResponse: res }))
+      // .catch(err => err);
   }
 
-  componentWillMount() {
-    this.callAPI();
+  componentDidMount() {
+    this._asyncRequest = this.callAPI().then(externalData => externalData.text()).then(res => {
+      this._asyncRequest = null;
+      this.setState({apiResponse: res});
+    })
+  }
+    // this.callAPI();
+  // }
+
+  componentWillUnmount() {
+    if (this._asyncRequest) {
+      this._asyncRequest.cancel();
+    }
   }
 
   render() {

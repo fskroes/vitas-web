1830caf3707f02b6fc29d754a05045e17ae2e46f
Fernando Silva Kroes <meanmachine200+github@gmail.com>
2020-11-04 16:14:09 +0100

    Rewrite page to use React hooks

--- src/App.js
+++ src/App.js
@@ -2,48 +2,69 @@ import './App.css';
 import React from 'react';
 import Upload from './component/upload'
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { 
-      apiResponse: '',
-      apiModelResponse: ''
-    };
-  }
 
-  callAPI() {
-    return fetch('http://localhost:9000/testAPI')
-  }
+function App() {
+  const [isAPIAlive, setApiAlive] = React.useState(false);
+  const [isAPIModelResponse, setAPIModelResponse] = React.useState(false);
 
-  // https://reactjs.org/blog/2018/03/27/update-on-async-rendering.html
-  componentDidMount() {
-    this._asyncRequest = this.callAPI()
-      .then(externalData => externalData.text())
-      .then(res => {
-        this._asyncRequest = null;
-        this.setState({apiResponse: res});
-    })
-  }
+  React.useEffect(() => {
+    fetch('http://localhost:9000/testAPI')
+      .then(res => res.text())
+      .then(res => setApiAlive(true))
+      .catch(err => setApiAlive(false))
+  }, [isAPIAlive]);
 
-  componentWillUnmount() {
-    if (this._asyncRequest) {
-      this._asyncRequest.cancel();
-    }
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <p className='App-intro'>
-          {this.state.apiResponse}
-          {this.state.apiModelResponse}
-        </p>
-        <div className="Card">
-          <Upload />
-        </div>
-      </div>
-    );
-  }
+  if (!isAPIAlive) return <p>API is alive.</p>
+  return(
+    <div className="Card">
+      <Upload />
+    </div>
+  )
 }
+
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { 
+//       apiResponse: '',
+//       apiModelResponse: ''
+//     };
+//   }
+
+//   callAPI() {
+//     return fetch('http://localhost:9000/testAPI')
+//   }
+
+//   // https://reactjs.org/blog/2018/03/27/update-on-async-rendering.html
+//   componentDidMount() {
+//     this._asyncRequest = this.callAPI()
+//       .then(externalData => externalData.text())
+//       .then(res => {
+//         this._asyncRequest = null;
+//         this.setState({apiResponse: res});
+//     })
+//   }
+
+//   componentWillUnmount() {
+//     if (this._asyncRequest) {
+//       this._asyncRequest.cancel();
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <p className='App-intro'>
+//           {this.state.apiResponse}
+//           {this.state.apiModelResponse}
+//         </p>
+//         <div className="Card">
+//           <Upload />
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;

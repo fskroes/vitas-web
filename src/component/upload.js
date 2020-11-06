c6e4b102efab4eec5f55e5ca64afbd4710a152f5
Fernando Silva Kroes <meanmachine200+github@gmail.com>
2020-11-06 23:35:43 +0100

    removed dep

--- src/component/upload.js
+++ src/component/upload.js
@@ -16,7 +16,6 @@ import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import useStyles from '../component/style';
 
 
 

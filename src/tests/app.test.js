f8dff03fa6c47a162fbc74776b840bdc75134bec
Fernando Silva Kroes <meanmachine200+github@gmail.com>
2020-11-27 12:44:46 +0100

    Commented redundant test

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -6,11 +6,11 @@ it("renders without crashing", () => {
     shallow(<App />);
 });
 
-it("renders Account header", () => {
-    const wrapper = shallow(<App />);
-    const welcome = <p>API is alive.</p>;
-    expect(wrapper.contains(welcome)).toEqual(true);
-});
+// it("renders Account header", () => {
+//     const wrapper = shallow(<App />);
+//     const welcome = <p>API is alive.</p>;
+//     expect(wrapper.contains(welcome)).toEqual(true);
+// });
 
 it('should render correctly with no props', () => {
     const component = shallow(<App/>);
